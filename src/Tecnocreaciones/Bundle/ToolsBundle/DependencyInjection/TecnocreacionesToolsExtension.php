754be5f61513fbddad6fe9737cc76d3e57bac05b
Carlos Mendoza <inhack20@gmail.com>
2014-03-27 13:26:28 -0430

    Creando configuracion de tecnocreacionestools

---
+++ src/Tecnocreaciones/Bundle/ToolsBundle/DependencyInjection/TecnocreacionesToolsExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tecnocreaciones\Bundle\ToolsBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
+class TecnocreacionesToolsExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+        
+        if($config['use_table_prefix']){
+            $tablePrefix = $config['table_prefix'].$config['table_prefix_separator'];
+            $tablePrefixListerner = new \Symfony\Component\DependencyInjection\Definition($container->getParameter('tecnocreaciones_tools.table_prefix_listerner.class'));
+            $tablePrefixListerner
+                    ->addArgument($tablePrefix)
+                    ->addTag('doctrine.event_subscriber')
+                    ;
+            $container->setDefinition('tecnocreaciones_tools.table_prefix_subscriber', $tablePrefixListerner);
+        }
+    }
+}
