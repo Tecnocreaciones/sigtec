042405a9616df78c9bcd5b6095535043284b2515
Carlos Mendoza <inhack20@gmail.com>
2014-03-28 09:23:55 -0430

    Añadiendo reglas de serializer

---
+++ src/Coramer/Sigtec/CompanyBundle/Controller/CompanyController.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com.ve
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Coramer\Sigtec\CompanyBundle\Controller;
+
+/**
+ * Description of CompanyController
+ *
+ * @author Carlos Mendoza <inhack20@tecnocreaciones.com.ve>
+ */
+class CompanyController extends \Sylius\Bundle\ResourceBundle\Controller\ResourceController
+{
+    public function indexAction(\Symfony\Component\HttpFoundation\Request $request) {
+        $criteria = $this->config->getCriteria();
+        $sorting = $this->config->getSorting();
+
+        $repository = $this->getRepository();
+
+        if ($this->config->isPaginated()) {
+            $resources = $this->resourceResolver->getResource(
+                $repository,
+                'createPaginator',
+                array($criteria, $sorting)
+            );
+            $resources->setCurrentPage($request->get('page', 1), true, true);
+            $resources->setMaxPerPage($this->config->getPaginationMaxPerPage());
+        } else {
+            $resources = $this->resourceResolver->getResource(
+                $repository,
+                'findBy',
+                array($criteria, $sorting, $this->config->getLimit())
+            );
+        }
+
+        $view = $this
+            ->view()
+            ->setTemplate($this->config->getTemplate('index.html'))
+            ->setTemplateVar($this->config->getPluralResourceName())
+            ->setData($resources)
+        ;
+        return $this->handleView($view);
+    }
+}
