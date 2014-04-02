f4337487054dea78ce71b99f4ed9487fd65c7b40
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-04-02 09:49:09 -0430

    Integrando tabla

--- web/angular/app/js/controllers.js
+++ web/angular/app/js/controllers.js
@@ -6,17 +6,9 @@ angular.module('sigtecModule.controllers', []).
   controller('ShowCompanyList', ['$scope','$http','notificationBarService',function($scope,$http,notificationBarService) {
           $scope.data = {};
           $scope.limit = '40';
-          $scope.evalFooter = function(type,footer){
-              for (var i = 0; i < footer.length; i++) {
-                    if (footer[i] === type) {
-                        return true;
-                    }
-                }
-              return false;
-          }
+          
           var url = Routing.generate('coramer_sigtec_company_index',{_format:'json'});
           return $http.get(url).success(function(data) {
-            console.log(data);
             $scope.data = data;
             
         }).error(function(data){
