3a0b1b130408eb7a92822b03ac8b732b73bccc39
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-04-03 08:33:26 -0430

    Ventana y formulario modal al eliminar un elemento

---
+++ src/Coramer/Sigtec/CoreBundle/Twig/Extension/CoreExtension.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com.ve
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Coramer\Sigtec\CoreBundle\Twig\Extension;
+
+/**
+ * Description of CoreExtension
+ *
+ * @author inhack20
+ */
+class CoreExtension extends \Twig_Extension
+{
+    
+    public function getFilters() {
+        return array(
+            new \Twig_SimpleFilter('hash', array($this, 'generateHash'), array('is_safe' => array('html'))),
+        );
+    }
+    
+    function generateHash($str) {
+        return md5($str);
+    }
+    
+    public function getName() {
+        return 'sigtec_core_extension';
+    }
+}
