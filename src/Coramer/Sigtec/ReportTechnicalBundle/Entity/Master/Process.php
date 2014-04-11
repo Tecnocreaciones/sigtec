5abcf1c4317600a0d09bd8f073c0a3b2caef226b
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-04-11 13:37:16 -0430

    Creando entidades del reporte tecnico

---
+++ src/Coramer/Sigtec/ReportTechnicalBundle/Entity/Master/Process.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Coramer\Sigtec\ReportTechnicalBundle\Entity\Master;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Process
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Coramer\Sigtec\ReportTechnicalBundle\Repository\Master\ProcessRepository")
+ */
+class Process
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=100)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="form", type="string", length=100)
+     */
+    private $form;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="equation", type="string", length=255)
+     */
+    private $equation;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active = true;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     */
+    private $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Process
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set form
+     *
+     * @param string $form
+     * @return Process
+     */
+    public function setForm($form)
+    {
+        $this->form = $form;
+
+        return $this;
+    }
+
+    /**
+     * Get form
+     *
+     * @return string 
+     */
+    public function getForm()
+    {
+        return $this->form;
+    }
+
+    /**
+     * Set equation
+     *
+     * @param string $equation
+     * @return Process
+     */
+    public function setEquation($equation)
+    {
+        $this->equation = $equation;
+
+        return $this;
+    }
+
+    /**
+     * Get equation
+     *
+     * @return string 
+     */
+    public function getEquation()
+    {
+        return $this->equation;
+    }
+}
---
+++ src/Coramer/Sigtec/ReportTechnicalBundle/Entity/Master/Resin.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Coramer\Sigtec\ReportTechnicalBundle\Entity\Master;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+/**
+ * Resin
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Coramer\Sigtec\ReportTechnicalBundle\Repository\Master\ResinRepository")
+ */
+class Resin
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=100)
+     */
+    private $description;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="active", type="boolean")
+     */
+    private $active = true;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     * @Gedmo\Timestampable(on="create")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updatedAt", type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     */
+    private $updatedAt;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Resin
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
