7a2412488855e8b49ded40c57e97db1be3ee389a
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-04-23 08:19:10 -0430

    Definiendo metodos de la interfaz del reporte tecnico

--- src/Coramer/Sigtec/ReportTechnicalBundle/Entity/ReportTechnicalInterface.php
+++ src/Coramer/Sigtec/ReportTechnicalBundle/Entity/ReportTechnicalInterface.php
@@ -33,4 +33,94 @@ interface ReportTechnicalInterface
      * Status rejected
      */
     const STATUS_REJECTED = 3;
+    
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return ReportTechnical
+     */
+    public function setCreatedAt($createdAt);
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt();
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return ReportTechnical
+     */
+    public function setUpdatedAt($updatedAt);
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime 
+     */
+    public function getUpdatedAt();
+
+    /**
+     * Set deletedAt
+     *
+     * @param string $deletedAt
+     * @return ReportTechnical
+     */
+    public function setDeletedAt($deletedAt);
+
+    /**
+     * Get deletedAt
+     *
+     * @return string 
+     */
+    public function getDeletedAt();
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return ReportTechnical
+     */
+    public function setStatus($status);
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus();
+
+    /**
+     * Set company
+     *
+     * @param \Coramer\Sigtec\CompanyBundle\Entity\Company $company
+     * @return ReportTechnical
+     */
+    public function setCompany(\Coramer\Sigtec\CompanyBundle\Entity\Company $company = null);
+
+    /**
+     * Get company
+     *
+     * @return \Coramer\Sigtec\CompanyBundle\Entity\Company 
+     */
+    public function getCompany();
+
+    /**
+     * Set archive
+     *
+     * @param string $archive
+     * @return ReportTechnical
+     */
+    public function setArchive($archive);
+
+    /**
+     * Get archive
+     *
+     * @return string 
+     */
+    public function getArchive();
 }
