4e55d52ffbfaa127ee4b69defeebed99e0b43284
Carlos Mendoza <inhack20@tecnocreaciones.com>
2014-05-13 20:04:40 -0430

    agregando nueva entidad o propiedad del reporte tecnico

--- src/Coramer/Sigtec/ReportTechnicalBundle/Entity/Master/Supplier.php
+++ src/Coramer/Sigtec/ReportTechnicalBundle/Entity/Master/Supplier.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
- * Supplier
+ * Proveedores de aditivos
  *
  * @ORM\Table()
  * @ORM\Entity(repositoryClass="Coramer\Sigtec\ReportTechnicalBundle\Repository\Master\SupplierRepository")
